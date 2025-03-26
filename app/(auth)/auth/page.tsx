7010eaf8deffb81b0688e320261a5dcbba20c0db
MrRedeks <m2208442@edu.misis.ru>
2025-03-26 13:37:16 +0300

    Добавил drop-shadow

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -2,10 +2,10 @@ import { AuthForm } from '@/components/custom/AuthForm'
 
 export default function AuthPage() {
   return (
-    <div className="w-full grid min-h-svh bg-gray-50">
+    <div className="w-full grid min-h-svh">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-md bg-white rounded-3xl shadow-sm p-8">
+          <div className="w-full max-w-md bg-white rounded-3xl drop-shadow-2xl p-8">
             <AuthForm />
           </div>
         </div>
